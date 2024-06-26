62ef57e05bc6e5c5e17e0497b65162e8aed8cfc4
CrozCamel <carlylecamelo@gmail.com>
2024-06-26 10:22:18 -0300

    Adding DatabseHelper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
 }
---
+++ app/src/main/java/com/example/task_three_sqlite3/Contact.kt
@@ -0,0 +1,7 @@
+package com.example.task_three_sqlite3
+
+data class Contact(
+    val id: Long = -1,
+    val name: String,
+    val phoneNumber: String
+)
